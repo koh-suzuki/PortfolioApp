d4c925b3e57513ec68a455f1ece32d9a40849f4f
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-04-28 06:14:51 +0000

    title 追加

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module PortfolioApp
     config.i18n.default_locale = :ja
     # タイムゾーンも変更するなら，以下を追加
     config.time_zone = 'Asia/Tokyo'
+    # logger 
+    # config.logger = Logger.new(STDOUT)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
