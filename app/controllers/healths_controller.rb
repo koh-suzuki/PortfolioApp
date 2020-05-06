2dad0012834a7eeb51b04cabf09c0860009a14a4
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-05-06 10:37:08 +0900

    diary showページ view調整

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -9,22 +9,6 @@ class HealthsController < ApplicationController
                                      ]
   before_action :authenticate_user!
 
-  def index
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
-  end
-
-  def new
-    @health = Health.new
-  end
-  
-  def create
-    @health = Health.create(health_params)
-    if @health.save
-      redirect_to 
-    else
-    end
-  end
 
   def edit
     @profile = Profile.find_by(user_id: current_user.id)
