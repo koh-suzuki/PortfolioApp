695a89c54ce629e7638c6fcbc9f4a870cd8aab4a
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-05-10 00:41:47 +0900

    omniauth_callback リダイレクト パス生成確認の削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -31,7 +31,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      puts "==========#{@user}============="
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
