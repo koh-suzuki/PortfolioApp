52969eba91d61b15826c4e21e316f382a666fbd1
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-05-09 14:45:10 +0900

    Diary 一覧に日記が無い場合は一覧コードを走らせない

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,9 +260,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2,Rails.application.credentials.aws[:GOOGLE_CLIENT_ID],Rails.application.credentials.aws[:GOOGLE_CLIENT_SECRET], scope: 'email', redirect_uri: "https://localhost:8080/users/auth/google_oauth2/callback"
+  config.omniauth :google_oauth2,Rails.application.credentials.aws[:GOOGLE_CLIENT_ID],Rails.application.credentials.aws[:GOOGLE_CLIENT_SECRET], scope: 'email', redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
   # "#{ENV['HOST']}/users/auth/google_oauth2/callback"
-  # https://lit-falls-19274.herokuapp.com
   OmniAuth.config.logger = Rails.logger if Rails.env.development? # debug用
 
   # ==> Warden configuration
