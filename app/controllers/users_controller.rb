a7c27b5572921a1c385199ebf264fb5b0e94ff80
koh-suzuki <k.s-coward-0629@ezweb.ne.jp>
2020-05-10 19:33:38 +0900

    グーグルログイン後の名前確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
       @profile.user_id = current_user.id
       @profile.save
     end
+    puts "================ #{@profile.name} ======================"
     @user_data = @healths.where.not(weight: nil)
     weights = @healths.pluck(:weight)
     unless weights.all?(&:nil?)
